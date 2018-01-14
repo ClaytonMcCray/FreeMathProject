ebf878f788926994acb9568cfbe01286c6e2a236
Clayton McCray <cpmdmccray@gmail.com>
2018-01-14 15:12:21 -0600

    added documentation to the Function class

--- FreeMath/functions.py
+++ FreeMath/functions.py
@@ -5,6 +5,8 @@ from typing import List, Union, NewType
 scalar = NewType('scalar', Union[float, int])
 
 
+# functions should be passed as strings containing variables if desired. As in, 'x ** 2'.
+# Strings should follow standard python mathematical notation.
 class Function:
     def __init__(self, func: str):
         self.func = func
@@ -12,6 +14,9 @@ class Function:
     def get_function(self):
         return self.func
 
+    # variables should be provided in a nested list: [['x', 2]] would evaluate the sentence
+    # for x = 2. The double nesting should be used even if only a single pair of arguments is passed.
+    # for more arguments, just extend the list: [['x', 2], ['y', 3], ['z', 4]].
     def evaluate(self, args: List) -> scalar:
         sentence = self.get_function()
         for var in args:
